e7293fb66ba91897d31450945db9b641a6a752fd
Iambizi <amirbzmn@gmail.com>
2022-08-05 13:39:07 -0400

    Saves changes

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+};
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
