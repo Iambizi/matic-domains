c4acbaaaa12a22ba3f22f2664c7d3eb3f9aecc0e
Iambizi <amirbzmn@gmail.com>
2022-08-08 14:19:15 -0400

    Adds String Utils library

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+// Don't forget to add this import
+import { StringUtils } from "../libraries/StringUtils.sol";
+
 // Import this file to use console.log
 import "hardhat/console.sol";
 
 contract Domains {
+
+  // Here's our domain TLD!
+  string public tld;
     
   // A "mapping" data type to store their names
   mapping(string => address) public domains;
@@ -16,10 +22,36 @@ contract Domains {
     console.log("YO YO ME A SMART CONTRACT I AM.");
   }
 
+  // We make the contract "payable" by adding this to the constructor
+  constructor(string memory _tld) payable {
+    tld = _tld;
+    console.log("%s name service deployed", _tld);
+  }
+		
+  // This function will give us the price of a domain based on length
+  function price(string calldata name) public pure returns(uint) {
+    uint len = StringUtils.strlen(name);
+    require(len > 0);
+    if (len == 3) {
+      return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+    } else if (len == 4) {
+      return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+    } else {
+      return 1 * 10**17;
+    }
+  }
+
   // A register function that adds their names to our mapping
   function register(string calldata name) public {
-      domains[name] = msg.sender;
-      console.log("%s has registered a domain!", msg.sender);
+    require(domains[name] == address(0));
+    
+    uint _price = price(name);
+
+    // Check if enough Matic was paid in the transaction
+    require(msg.value >= _price, "Not enough Matic paid");
+
+    domains[name] = msg.sender;
+    console.log("%s has registered a domain!", msg.sender);
   }
 
   // This will give us the domain owners' address
