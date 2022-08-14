7fd14d47bdab0067349f88633fea9b01baace89f
Iambizi <amirbzmn@gmail.com>
2022-08-14 10:08:25 -0400

    creates deploy script file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,17 @@
 
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+// require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: `${process.env.ALCHEMY_MUMBAI_URL}`,
+      accounts: `0x${process.env.PRIVATE_KEY}`,
+    }
+  }
 };
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
